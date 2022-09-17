41d60860fddc177d28091742053d80977a785627
kishii4726 <ikawszaq12wsx@gmail.com>
2022-09-17 22:12:00 +0900

    us-east-1のcfg追加

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -17,3 +17,13 @@ func LoadConfig() aws.Config {
 
 	return cfg
 }
+
+func UsEast1LoadConfig() aws.Config {
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	return cfg
+}
