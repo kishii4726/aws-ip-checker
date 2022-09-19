4c9e0f8a33ed4364f5b373ec4696aca13061bbef
kishii4726 <ikawszaq12wsx@gmail.com>
2022-09-19 11:30:46 +0900

    NextTokenの処理追加

--- pkg/aws/service/alb/alb.go
+++ pkg/aws/service/alb/alb.go
@@ -4,31 +4,58 @@ import (
 	"aws-ip-checker/pkg/utils"
 	"context"
 	"log"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
 )
 
-func GetLoadBalancers(client *elasticloadbalancingv2.Client) *elasticloadbalancingv2.DescribeLoadBalancersOutput {
-	resp, err := client.DescribeLoadBalancers(context.TODO(), &elasticloadbalancingv2.DescribeLoadBalancersInput{
+func GetLoadBalancers(client *elasticloadbalancingv2.Client) [][]string {
+	var d [][]string
+	req_params := &elasticloadbalancingv2.DescribeLoadBalancersInput{
 		PageSize: aws.Int32(100),
-	})
-	if err != nil {
-		log.Fatal(err)
 	}
 
-	return resp
+	for {
+		resp, err := client.DescribeLoadBalancers(context.TODO(), req_params)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range resp.LoadBalancers {
+			d = append(d, []string{*v.LoadBalancerName, *v.LoadBalancerArn})
+		}
+		if resp.NextMarker != nil {
+			req_params.Marker = resp.NextMarker
+		} else {
+			break
+		}
+	}
+
+	return d
 }
 
-func DescribeLoadBalancerListeners(client *elasticloadbalancingv2.Client, load_balancer_arn *string) *elasticloadbalancingv2.DescribeListenersOutput {
-	resp, err := client.DescribeListeners(context.TODO(), &elasticloadbalancingv2.DescribeListenersInput{
+func DescribeLoadBalancerListeners(client *elasticloadbalancingv2.Client, load_balancer_arn *string) [][]string {
+	var d [][]string
+	req_params := &elasticloadbalancingv2.DescribeListenersInput{
 		LoadBalancerArn: load_balancer_arn,
-	})
-	if err != nil {
-		log.Fatal(err)
 	}
 
-	return resp
+	for {
+		resp, err := client.DescribeListeners(context.TODO(), req_params)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range resp.Listeners {
+			d = append(d, []string{*v.ListenerArn, strconv.Itoa(int(*v.Port))})
+		}
+		if resp.NextMarker != nil {
+			req_params.Marker = resp.NextMarker
+		} else {
+			break
+		}
+	}
+
+	return d
 }
 
 func CheckContainIpAddress(client *elasticloadbalancingv2.Client, listen_arn *string, ipaddresses []string) []string {
